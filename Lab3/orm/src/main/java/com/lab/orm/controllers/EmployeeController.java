ae887ef39cd0a286b6eee7f239dcd97b6f9d604d
AdamHtooLwin <adamhtoolwin@gmail.com>
2021-03-05 23:14:07 +0700

    Add controller

---
+++ Lab3/orm/src/main/java/com/lab/orm/controllers/EmployeeController.java
@@ -0,0 +1,27 @@
+package com.lab.orm.controllers;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.lab.orm.models.Employee;
+import com.lab.orm.repo.EmployeeRepo;
+
+
+@RestController
+@RequestMapping(path = "/employees")
+public class EmployeeController {
+
+    @Autowired
+    EmployeeRepo empRepo;
+
+    @GetMapping("/employee/{id}")
+    public Employee getEmployee(@PathVariable int id) {    	
+    	Employee emp = empRepo.findById(id).orElse(null);
+        return emp;
+       
+   }
+
+}
